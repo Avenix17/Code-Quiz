4269cbec1ca7c09005384f6ef4dd1ad8c82efe2e
Avyrie Fellows <avyriefellows@gmail.com>
2022-05-14 19:15:49 -0600

    Created createdQuestions object array and functions for start, show question, and next question

--- script.js
+++ script.js
@@ -1,35 +1,112 @@
-const start = document.getElementById("start")
-const quizQuestions = document.getElementById("quiz-questions")
-var questions = document.getElementById("questions")
-var answerButtons = document.getElementById("answer-buttons")
+const start = document.getElementById("start");
+const quizQuestions = document.getElementById("quiz-questions");
+var questions = document.getElementById("questions");
+var answerButtons = document.getElementById("answer-buttons");
+var highScoreSubmission = document.getElementById("enter-high-scores")
 
-var randomizedQuestions
-var currentQuestion
+var randomizedQuestions;
+var currentQuestion;
 
 var sec = 90;
-var time = setInterval(timer, 1000);
+var time;
+var options;
+var question;
 
-var createdQuestions = []
+const a1 = document.getElementById('a1');
+const a2 = document.getElementById('a2');
+const a3 = document.getElementById('a3');
+const a4 = document.getElementById('a4');
 
-start.addEventListener("click", startQuiz)
+var createdQuestions = [
+    {   question: "You end every line in CSS with what...",
+        options: [
+            {text: "A semicolon", correct: true},
+            {text: "I dunno", correct: false},
+            {text: "Jeffrey Bezos", correct: false},
+            {text: "Nothing", correct: false},
+        ]
+    },
+    {   question: "Billy joe sally",
+        options: [
+            {text: "cotton eye joe", correct: true},
+            {text: "billy jean is not my lover", correct: false},
+            {text: "sally mae", correct: false},
+            {text: "Hua?", correct: false},
+        ]
+    },
+    {   question: "Billy joe ",
+        options: [
+            {text: "cotton eye joe", correct: true},
+            {text: "billy jean is not my lover", correct: false},
+            {text: "sally mae", correct: false},
+            {text: "Hua?", correct: false},
+        ]
+    },
+    {   question: "Billy joe richy",
+        options: [
+            {text: "cotton eye joe", correct: true},
+            {text: "billy jean is not my lover", correct: false},
+            {text: "sally mae", correct: false},
+            {text: "Hua?", correct: false},
+        ]
+    },
+    {   question: "Billy joe crazy",
+        options: [
+            {text: "cotton eye joe", correct: true},
+            {text: "billy jean is not my lover", correct: false},
+            {text: "sally mae", correct: false},
+            {text: "Hua?", correct: false},
+        ]
+    },
+]
+
+start.addEventListener("click", startQuiz);
 
 function startQuiz() {
+    time = setInterval(timer, 1000);
     start.classList.add("hide");
+    randomizedQuestions = createdQuestions.sort(() => Math.random() > .5 ? 1 : -1);
+    currentQuestion = 0;
     quizQuestions.classList.remove("hide");
-}
-
-function showQuestion() {
-}
-
-function correctAnswer() {
-}
-
-function incorrectAnswer() {
+    nextQuestion();
+    a1.addEventListener("click", chooseAnswer);
+    a2.addEventListener("click", chooseAnswer);
+    a3.addEventListener("click", chooseAnswer);
+    a4.addEventListener("click", chooseAnswer);
 }
 
 function nextQuestion() {
+    showQuestion(randomizedQuestions[currentQuestion]);
 }
 
+function showQuestion(question) {
+    questions.innerText = question.question;
+
+    let options = createdQuestions[currentQuestion].options
+    options.sort(() => Math.random() > .5 ? 1 : -1);
+    a1.textContent = options[0].text;
+    a2.textContent = options[1].text;
+    a3.textContent = options[2].text;
+    a4.textContent = options[3].text;
+
+    a1.value = options[0].correct;
+    a2.value = options[1].correct;
+    a3.value = options[2].correct;
+    a4.value = options[3].correct;
+
+}
+
+function chooseAnswer(event) {
+    if (event.target.value) {
+    // TODO: correct answer
+    // Correct! move to next question
+    } else {
+    // TODO: incorrect answer
+    // deduct 15 sec
+    // Wrong! move to next question
+
+    }
+}
 
 function timer() {
     document.getElementById("time-left").innerHTML = sec;
@@ -40,6 +117,4 @@ function timer() {
     }
 }
 
-function leaderBoard() {
 
-}
